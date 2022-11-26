bb0c569701e37dcee5c9cebb797c9641e0da8221
adnan <adnanfahedhr@gmail.com>
2022-11-26 12:08:36 +0300

    init clean script

---
+++ scripts/clean.sh
@@ -0,0 +1,18 @@
+clean() {
+    cd $1
+    flutter clean
+    rm -r .dart_tool
+    rm -r android/.gradle
+    rm -rf android/gradlew
+    rm -rf android/gradlew.bat
+    rm -rf android/local.properties
+    rm -rf ios/.symlinks
+    rm -rf ios/Pods
+    rm -rf pubspec.lock
+    flutter pub get
+    cd ../
+}
+
+clean data/
+clean domain/
+clean feature/
